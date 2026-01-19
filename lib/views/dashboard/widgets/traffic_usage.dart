3cce25dcddfdeed77f8afb1f983e958b11518e4a
ibettyou <appshubcc@gmail.com>
2026-01-19 12:23:09 +0800

    fix widgets & build

--- lib/views/dashboard/widgets/traffic_usage.dart
+++ lib/views/dashboard/widgets/traffic_usage.dart
@@ -62,157 +62,158 @@ class TrafficUsage extends StatelessWidget {
             iconData: Icons.data_saver_off,
           ),
           onPressed: () {},
-        child: Consumer(
-          builder: (_, ref, __) {
-            final totalTraffic = ref.watch(totalTrafficProvider);
-            final upTotalTrafficValue = totalTraffic.up;
-            final downTotalTrafficValue = totalTraffic.down;
-            return Padding(
-              padding: baseInfoEdgeInsets.copyWith(
-                top: 0,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Flexible(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 12,
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: DonutChart(
-                              data: [
-                                DonutChartData(
-                                  value: upTotalTrafficValue.value.toDouble(),
-                                  color: primaryColor,
-                                ),
-                                DonutChartData(
-                                  value: downTotalTrafficValue.value.toDouble(),
-                                  color: secondaryColor,
-                                ),
-                              ],
+          child: Consumer(
+            builder: (_, ref, __) {
+              final totalTraffic = ref.watch(totalTrafficProvider);
+              final upTotalTrafficValue = totalTraffic.up;
+              final downTotalTrafficValue = totalTraffic.down;
+              return Padding(
+                padding: baseInfoEdgeInsets.copyWith(
+                  top: 0,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 12,
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            AspectRatio(
+                              aspectRatio: 1,
+                              child: DonutChart(
+                                data: [
+                                  DonutChartData(
+                                    value: upTotalTrafficValue.value.toDouble(),
+                                    color: primaryColor,
+                                  ),
+                                  DonutChartData(
+                                    value: downTotalTrafficValue.value.toDouble(),
+                                    color: secondaryColor,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Flexible(
-                            child: LayoutBuilder(
-                              builder: (_, container) {
-                                final uploadText = Text(
-                                  maxLines: 1,
-                                  appLocalizations.upload,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: context.textTheme.bodySmall,
-                                );
-                                final downloadText = Text(
-                                  maxLines: 1,
-                                  appLocalizations.download,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: context.textTheme.bodySmall,
-                                );
-                                final uploadTextSize = globalState.measure
-                                    .computeTextSize(uploadText);
-                                final downloadTextSize = globalState.measure
-                                    .computeTextSize(downloadText);
-                                final maxTextWidth = max(uploadTextSize.width,
-                                    downloadTextSize.width);
-                                if (maxTextWidth + 24 > container.maxWidth) {
-                                  return Container();
-                                }
-                                return Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Container(
-                                          width: 20,
-                                          height: 8,
-                                          decoration: BoxDecoration(
-                                            color: primaryColor,
-                                            borderRadius:
-                                                BorderRadius.circular(3),
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: 4,
-                                        ),
-                                        Text(
-                                          maxLines: 1,
-                                          appLocalizations.upload,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: context.textTheme.bodySmall,
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: 4,
-                                    ),
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Container(
-                                          width: 20,
-                                          height: 8,
-                                          decoration: BoxDecoration(
-                                            color: secondaryColor,
-                                            borderRadius:
-                                                BorderRadius.circular(3),
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: 4,
-                                        ),
-                                        Text(
-                                          maxLines: 1,
-                                          appLocalizations.download,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: context.textTheme.bodySmall,
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                );
-                              },
+                            SizedBox(
+                              width: 8,
                             ),
-                          ),
-                        ],
+                            Flexible(
+                              child: LayoutBuilder(
+                                builder: (_, container) {
+                                  final uploadText = Text(
+                                    maxLines: 1,
+                                    appLocalizations.upload,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: context.textTheme.bodySmall,
+                                  );
+                                  final downloadText = Text(
+                                    maxLines: 1,
+                                    appLocalizations.download,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: context.textTheme.bodySmall,
+                                  );
+                                  final uploadTextSize = globalState.measure
+                                      .computeTextSize(uploadText);
+                                  final downloadTextSize = globalState.measure
+                                      .computeTextSize(downloadText);
+                                  final maxTextWidth = max(uploadTextSize.width,
+                                      downloadTextSize.width);
+                                  if (maxTextWidth + 24 > container.maxWidth) {
+                                    return Container();
+                                  }
+                                  return Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Container(
+                                            width: 20,
+                                            height: 8,
+                                            decoration: BoxDecoration(
+                                              color: primaryColor,
+                                              borderRadius:
+                                                  BorderRadius.circular(3),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 4,
+                                          ),
+                                          Text(
+                                            maxLines: 1,
+                                            appLocalizations.upload,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: context.textTheme.bodySmall,
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Container(
+                                            width: 20,
+                                            height: 8,
+                                            decoration: BoxDecoration(
+                                              color: secondaryColor,
+                                              borderRadius:
+                                                  BorderRadius.circular(3),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 4,
+                                          ),
+                                          Text(
+                                            maxLines: 1,
+                                            appLocalizations.download,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: context.textTheme.bodySmall,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  _buildTrafficDataItem(
-                    context,
-                    Icon(
-                      Icons.arrow_upward,
-                      color: primaryColor,
-                      size: 14,
+                    _buildTrafficDataItem(
+                      context,
+                      Icon(
+                        Icons.arrow_upward,
+                        color: primaryColor,
+                        size: 14,
+                      ),
+                      upTotalTrafficValue,
                     ),
-                    upTotalTrafficValue,
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  _buildTrafficDataItem(
-                    context,
-                    Icon(
-                      Icons.arrow_downward,
-                      color: secondaryColor,
-                      size: 14,
+                    const SizedBox(
+                      height: 8,
                     ),
-                    downTotalTrafficValue,
-                  )
-                ],
-              ),
-            );
-          },
+                    _buildTrafficDataItem(
+                      context,
+                      Icon(
+                        Icons.arrow_downward,
+                        color: secondaryColor,
+                        size: 14,
+                      ),
+                      downTotalTrafficValue,
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
